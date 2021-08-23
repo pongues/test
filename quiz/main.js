aec14ea26d234fb16af7ed90fc24dbb013b2cb08
pongues <74826137+pongues@users.noreply.github.com>
2021-08-23 20:52:55 +0900

    Add files via upload

---
+++ quiz/main.js
@@ -0,0 +1,27 @@
+const questions = [
+	{ buttonId: "button1", inputId: "input1", outputId: "output1", submitted: false, type: "text", answer: "ぽんゲス" },
+	{ buttonId: "button2", inputId: "input2", outputId: "output2", submitted: false, type: "text", answer: "ぽん" },
+];
+
+const output = document.getElementById("output");
+let point = 0;
+
+for(const q of questions){
+	const button = document.getElementById(q.buttonId);
+	button.addEventListener("click", function(e){
+		if(!q.submitted){
+			const inputTag = document.getElementById(q.inputId);
+			const outputTag = document.getElementById(q.outputId);
+			const value = inputTag.value;
+			if(value == q.answer){
+				outputTag.innerHTML = "<font class='correct'>正解</font><br/><font>君は偉い</font>"
+				point++;
+				output.innerText = point + "点";
+			}else{
+				outputTag.innerHTML = "<font class='incorrect'>不正解</font><br/><font>正解は「" + q.answer + "」</font>"
+			}
+			inputTag.readOnly = true;
+			q.submitted = true;
+		}
+	}, false);
+}
