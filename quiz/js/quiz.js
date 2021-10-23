33afd3d87ecfbf68950e94f1d4fd0b33bedf1ebb
pongues <74826137+pongues@users.noreply.github.com>
2021-10-23 23:50:54 +0900

    Add files via upload

---
+++ quiz/js/quiz.js
@@ -0,0 +1,57 @@
+const $list = document.getElementById("quizlist");
+
+const $inputs = new Array();
+const $answers = new Array();
+
+for(const quiz of quizzes){
+	console.log(quiz);
+	const $div = document.createElement("div");
+
+	const $title = document.createElement("div");
+	$title.setAttribute("class", "title");
+	$title.appendChild(document.createTextNode(quiz.title));
+	$div.appendChild($title);
+
+	if(quiz.type === "text"){
+		const $input = document.createElement("input");
+		$input.setAttribute("type", "text");
+		$div.appendChild($input);
+		$inputs.push($input);
+	}else if(quiz.type === "select"){
+		const $input = document.createElement("select");
+		for(const option of quiz.options){
+			const $option = document.createElement("option");
+			$option.appendChild(document.createTextNode(option));
+			$option.setAttribute("value", option);
+			$input.appendChild($option);
+		}
+		$div.appendChild($input);
+		$inputs.push($input);
+	}else{
+		$inputs.push(document.createElement("input"));
+	}
+
+	const $answer = document.createElement("div");
+	$div.appendChild($answer);
+	$answers.push($answer);
+
+	$div.appendChild(document.createElement("br"));
+	$list.appendChild($div);
+}
+
+const $button = document.getElementById("button");
+$button.addEventListener("click", function(e){
+	for(let i=0, l=quizzes.length; i<l; i++){
+		const quiz = quizzes[i];
+		const $input = $inputs[i];
+		const $answer = $answers[i];
+
+		if($input.value === quiz.answer){
+			$answer.appendChild(document.createTextNode("正解"));
+			$answer.setAttribute("class", "correct");
+		}else{
+			$answer.appendChild(document.createTextNode("不正解: " + quiz.answer));
+			$answer.setAttribute("class", "incorrect");
+		}
+	}
+});
